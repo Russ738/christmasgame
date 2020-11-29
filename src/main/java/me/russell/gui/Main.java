409d673ba66ff4d1d6f6cf187a5b5c114c8cf035
Russ738 <russjwelch@gmail.com>
2020-11-29 17:45:12 -0500

    First commit

---
+++ src/main/java/me/russell/gui/Main.java
@@ -0,0 +1,7 @@
+package me.russell.gui;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu.main(args);
+    }
+}
---
+++ src/main/java/me/russell/gui/Menu.java
@@ -0,0 +1,48 @@
+package me.russell.gui;
+
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+
+
+public class Menu extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        VBox pane = new VBox();
+        Button playButton = new Button();
+        playButton.setText("Play");
+        //Setting title to the Stage
+        stage.setTitle("Event Handlers Example");
+
+        //setting layout type
+        pane.setAlignment(Pos.CENTER);
+        pane.getChildren().addAll(playButton);
+        pane.setId("pane");
+
+        //Creating a scene object
+        Scene scene = new Scene(pane, 800, 500);
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        scene.setFill(Color.TRANSPARENT);
+
+        //Adding scene to the stage
+        stage.initStyle(StageStyle.TRANSPARENT);
+        //stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(scene);
+
+        //Displaying the contents of the stage
+        stage.show();
+    }
+    public static void main(String args[]){
+        launch(args);
+    }
+}
